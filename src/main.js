e217f51a55983b451e2a5f4b61afcebcb5c564c2
JunmanChoi <junman9595@gmail.com>
2024-12-16 04:09:01 +0900

    fix: 유닛 테스트에서 beforeAll을 통해서 매번 html초기화 하는 코드가 있어, main.js 에서 이벤트 리스너가 아닌 기본 html string을 제공토록 처리

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import router from "./network/router/Router";
-
+import MainPage from "./pages/app";
+document.body.innerHTML = MainPage();
 window.addEventListener("load", () => router.route(window.location.pathname));
