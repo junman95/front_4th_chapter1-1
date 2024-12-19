2e7698a0cb35ad0e9368b81bff93e751d0e9de58
JunmanChoi <junman9595@gmail.com>
2024-12-20 02:59:08 +0900

    fix: 렌더 속도를 저하 시키는 불필요한 페이지 렌더 로직 삭제

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import User from "./lib/auth/User";
 import router from "./network/router/Router";
-import MainPage from "./pages/app";
 import featureStore from "./stores/FeatureStore";
 import userStore from "./stores/UserStore";
 
@@ -8,7 +7,6 @@ const init = () => {
   userStore.setUser(User.getUser());
   document.body.innerHTML = `<div id="root"></div>`;
   featureStore.setFeature(window.location.pathname);
-  MainPage(document.body, "#root");
 };
 init();
 
