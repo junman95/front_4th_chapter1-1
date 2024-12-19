af0e360a118360ffa84599e6c109938a72063966
JunmanChoi <junman9595@gmail.com>
2024-12-19 21:09:45 +0900

    fix: 누락된 유저 정보 넣어주는 코드 복구

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import User from "./lib/auth/User";
 import router from "./network/router/Router";
 import MainPage from "./pages/app";
 import featureStore from "./stores/FeatureStore";
+import userStore from "./stores/UserStore";
 
 const init = () => {
+  userStore.setUser(User.getUser());
   document.body.innerHTML = `<div id="root"></div>`;
   featureStore.setFeature(window.location.pathname);
   MainPage(document.body, "#root");
